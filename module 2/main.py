c213bc4d3bf3bb531f1eb92d5333784f18bba4d2
Hasan Suxhuku <amarbuzoku123@gmail.com>
2026-01-26 19:16:06 +0100

    a

---
+++ module 2/main.py
@@ -0,0 +1,11 @@
+words = ("hello" , "hi" , "pershendetje")
+print(words(0))
+
+empty_lople = ()
+print(empty_lople)
+
+person = ("Amar"  , 14 , "pROGRAMER")
+
+name , age , profession = person
+
+print( name , "s", "profession is" , profession , "and he is", age)
