38ff2371e41ce713b759e049f833d5a464d079ee
Hasan Suxhuku <amarbuzoku123@gmail.com>
2026-01-28 19:07:57 +0100

    sdf
    dsf

---
+++ module3/conditionals.py
@@ -0,0 +1,22 @@
+age = 18
+
+if age >= 18 :
+    print("you can vote")
+else:
+    print("you cannot vote")
+
+temperature = 28
+
+ if temperature > 28
+     print("its hot")
+    elif 20 <= temperature <=30
+        print("pleasant")
+ else:
+     print("cold")
+
+     number = 7
+
+if number %2 == 0:
+    print("thenumber is even")
+    esle:
+    print("the number is odd")
