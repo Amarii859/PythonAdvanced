309cde4b50813c33d5dd0a8d5c03f40d9831116f
Hasan Suxhuku <amarbuzoku123@gmail.com>
2026-02-02 19:18:48 +0100

    ssdD
    ASSADSA

---
+++ challange.py
@@ -0,0 +1,17 @@
+n1 = 3.5
+pike = 65
+
+if n1 >= 3.5 and 50 <= pike <=65 :
+    print("half")
+elif pike > 65:
+    print("full")
+else:
+     print("cnat have a sc")
+
+
+else:
+print("cant have a sc")
+
+
+
+
